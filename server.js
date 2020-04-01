7ed76a9b6dbd22b139ef2d50dca71aa97d173484
Sárpátki Ádám <sepa1989@gmail.com>
2020-04-01 18:50:41 +0200

    feat: add weather journal app code

--- server.js
+++ server.js
@@ -2,18 +2,39 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
+const bodyParser = require('body-parser');
+
+// Configuring express to use body-parser as middle-ware
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+
+//Initializing server callback function
+app.listen(port, () => { console.log(`running on localhost: ${port}`) });
+
+//GET route that returns projectData
+app.get('/data', function (req, res) {
+    res.send(projectData);
+})
+
+//POST route that adds incoming data to projectData
+app.post('/data', function (req, res) {
+    projectData['temperature'] = req.body.temperature;
+    projectData['date'] = req.body.date;
+    projectData['userResponse'] = req.body.userResponse;
+    res.send({});
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,60 @@
 /* Global Variables */
+const baseURL = 'api.openweathermap.org/data/2.5/weather?';
+const appID = 'd29325eeedff19bec6e46a01c7124d3f';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const getData = async (url) => {
+  const response = await fetch(url, {
+    method: 'GET',
+    credentials: 'same-origin',
+  });
+
+  try {
+    const newData = response.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+
+  try {
+    const newData = response.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+const retreive = async () => {
+  const retreivedData = await getData('/data');
+  document.getElementById('date').innerHTML = retreivedData.date;
+  document.getElementById('temp').innerHTML = retreivedData.temperature;
+  document.getElementById('content').innerHTML = retreivedData.userResponse;
+}
+
+const sendData = async () => {
+  const zip = document.getElementById('zip');
+  const userResponse = document.getElementById('feelings');
+  let d = new Date();
+  let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+  const weatherData = await getData(`http://api.openweathermap.org/data/2.5/weather?zip=${zip.value},us&appid=${appID}`);
+  const data = {
+    temperature: weatherData.main.temp,
+    date: newDate,
+    userResponse: userResponse.value
+  };
+  await postData('/data', data);
+  retreive();
+}
+
+const generate = document.getElementById('generate');
+generate.addEventListener("click", sendData);
